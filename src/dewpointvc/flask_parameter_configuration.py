a35a707d93abb20de430d2b06b1594d9f3043746
Thomas Ebner <thomas.ebner@kolaido.com>
2024-11-20 16:34:17 +0100

    proper parsing of different types and error mesage

--- src/dewpointvc/flask_parameter_configuration.py
+++ src/dewpointvc/flask_parameter_configuration.py
@@ -2,7 +2,7 @@ import logging
 import os
 import threading
 
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 
 from dewpointvc.parameter_collection import ParameterCollection
 
@@ -13,6 +13,7 @@ class FlaskParameterConfiguration:
         self.app = Flask(__name__,
                          template_folder=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates'))
         self.setup_routes()
+        self.app.secret_key = "Eejeich6eT6hooMu"  # Required for flash messages
         self.logger = logging.getLogger(__name__)
 
     def setup_routes(self):
@@ -29,13 +30,24 @@ class FlaskParameterConfiguration:
             # Update all parameters based on form data
             for param, value in request.form.items():
                 default_value = self.config_service.get_param(param)
-                if isinstance(default_value, bool):
-                    value = value.lower() in ["true", "1"]
-                elif isinstance(default_value, int):
-                    value = int(value)
-                # Set the parameter with the updated value
-                self.config_service.set_param(param, value)
-                self.logger.info("Updated parameter '%s' to: %s", param, value)
+                try:
+                    if isinstance(default_value, bool):
+                        value = value.lower() in ["true", "1"]
+                    elif isinstance(default_value, int):
+                        value = int(value)
+                    elif isinstance(default_value, float):
+                        value = float(value)
+
+                    if value != default_value:
+                        self.logger.info(
+                            "Updated parameter '%s' from: %s to: %s", param, default_value, value
+                        )
+                    # Set the parameter with the updated value
+                    self.config_service.set_param(param, value)
+                    self.logger.info("Updated parameter '%s' to: %s", param, value)
+                except (ValueError, TypeError):
+                    flash(f"Invalid value for parameter '{param}': {value}", 'error')
+                    # return redirect(url_for('index'))
             return redirect(url_for('index'))
 
     def run(self):
